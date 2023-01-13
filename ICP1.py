d6b53ce8ec95e0ccbdeb52a2aebbdacf60151bee
yashwanth700743506 <122486156+yashwanth700743506@users.noreply.github.com>
2023-01-12 19:27:53 -0600

    Add files via upload

---
+++ ICP1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+#1Q
+str="python"
+str[:-2][::-1]
+
+
+# In[47]:
+
+
+#2Q
+A=8; B=6
+x=A+B #Addition
+x
+
+
+# In[48]:
+
+
+y=A-B #subtraction
+y
+
+
+# In[49]:
+
+
+o=A/B #Division
+o
+
+
+# In[50]:
+
+
+p=A*B #Multiplication
+p
+
+
+# In[52]:
+
+
+#3Q
+str="Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Python is dynamically-typed and garbage-collected."
+str.replace('Python','pythons')
+
+
+# In[54]:
+
+
+#4Q
+Score=float(input("Enter your score:"))
+if Score>90:
+    print("Grade:A")
+elif Score>=80 and Score<90:
+    print("Grade:B")
+elif Score>=70 and Score<80:
+    print("Grade:C")
+elif Score>=60 and Score<70:
+    print("Grade:D")
+elif Score<=59:
+    print("Grade:F")
+    
+
+
+# In[ ]:
+
+
+
+
